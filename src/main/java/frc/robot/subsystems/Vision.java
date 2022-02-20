ad55fa8641d32541f4b40ac98b1f42e8785bf877
samueldaley <stallionsamueld@gmail.com>
2022-02-19 20:59:02 -0500

    Fixed vision error of using tx instead of ty

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -7,19 +7,26 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Vision extends SubsystemBase {
     private final NetworkTableEntry txEntry;
+    private final NetworkTableEntry tyEntry;
     private static final double kCameraHeightMeters = 1;
     private static final double kCameraAngleDegrees = 0;
 
     public Vision() {
         this.txEntry = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx");
+        this.tyEntry = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty");
     }
 
     public double getTX() {
         return (double) this.txEntry.getNumber(0.0);
     }
 
+    public double getTY() {
+        return (double) this.txEntry.getNumber(0.0);
+    }
+
+    /** Calculates the distance from  */
     public double getDistanceFromTargetWithHeight(double heightMeters) {
-        double a2 = getTX();
+        double a2 = getTY();
         return (heightMeters - kCameraHeightMeters) / (Trajectory.tan(kCameraAngleDegrees) + a2);
     }
 }
