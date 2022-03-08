01f2c2c5cd094ad1cd74b1bf1eed353a1b9d603e
cwtech2959 <cwtech2959@gmail.com>
2022-03-08 18:19:07 -0500

    Adjust to new start position for front right wheel

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -61,7 +61,7 @@ public final class RobotMap {
 
     // Zeroed values, should be in radians
     public static final double kZeroedFrontLeft = 3.082;
-    public static final double kZeroedFrontRight = 0.000;
+    public static final double kZeroedFrontRight = 1.571;
     public static final double kZeroedBackLeft = 0.520;
     public static final double kZeroedBackRight = -0.230;
     public static final double kZeroedTurret = 0.0;
