5170c583472ac05402361375c1e09d0af7f7cbe0
Kevin Russell <kevinfrussell@gmail.com>
2022-03-20 14:42:50 -0400

    Fix shooter speed comparison for ending command

--- src/main/java/frc/robot/commands/SetShooterSpeedCommand.java
+++ src/main/java/frc/robot/commands/SetShooterSpeedCommand.java
@@ -73,7 +73,7 @@ public class SetShooterSpeedCommand extends CommandBase {
     public boolean isFinished() {
         // return Util.dcompareMine(m_container.shooter.getVelocity(), m_targetRpm, 300);
         if (m_targetRpm > 300)
-            return m_container.shooter.getVelocity() - 300 >= m_targetRpm;
+            return m_container.shooter.getVelocity() >= m_targetRpm - 300;
         else
             return m_container.shooter.getVelocity() < m_targetRpm + 300;
     }
